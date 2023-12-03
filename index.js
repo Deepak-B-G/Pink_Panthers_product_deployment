55ce963f9d767508a788bf4f447e10626d97cac8
Deepak-B-G <boppanadeepak1637@gmail.com>
2023-12-03 14:08:07 +0530

    Added heap js

--- index.js
+++ index.js
@@ -4,31 +4,23 @@ const fs = require('fs');
 const apiKey = 'phc_laVD3EFCGBc95FIudwgMobmsYwVtQiG4oSye0rTvxgS';
 const apiUrl = 'https://app.posthog.com/batch';
 // Read user details from the "person.json" file
-const userData = JSON.parse(fs.readFileSync('person2.json', 'utf8'));
+const userData = JSON.parse(fs.readFileSync('person4.json', 'utf8'));
 
-const totalEvents = 10000;
+const totalEvents = 5000;
 const events = [];
 
-const generateRandomTimestamp = () => {
-  const startDate = new Date('2023-01-01');
-  const endDate = new Date('2023-12-31');
-  const randomTimestamp = new Date(
-    startDate.getTime() + Math.random() * (endDate.getTime() - startDate.getTime())
-  );
-  return randomTimestamp.toISOString();
-};
+const fixedDate = new Date('2023-11-11T12:00:00'); // Updated fixed date and time
 
-// Generate and add "$pageview" events with user details
+// Generate and add "$v1" events with user details
 for (let i = 0; i < totalEvents; i++) {
   const user = userData[i % userData.length]; // Reuse user details cyclically
   const event = {
-    event: '$pageview',
+    event: '$Pageview',
     properties: {
       distinct_id: user.distinct_id,
       gender: user.gender,
       country: user.country,
-      browser: user.browser,
-      timestamp: generateRandomTimestamp(),
+      browser: user.browser
     },
   };
   events.push(event);
@@ -47,7 +39,7 @@ const sendEvents = async () => {
         'Content-Type': 'application/json',
       },
     });
-    console.log(`Sent ${totalEvents} "$pageview" events with user details in a single batch.`);
+    console.log(`Sent ${totalEvents} "$v1" events with user details in a single batch.`);
   } catch (error) {
     console.error(`Error sending events: ${error}`);
   }
--- user.js
+++ user.js
@@ -1,15 +1,13 @@
 const uuid = require('uuid');
 const fs = require('fs');
 
-// Number of users to generate
-const numUsers = 10000;
 
-// List of specified countries, genders, and browsers
+const numUsers = 5000;
+
 const countries = ['India', 'Australia', 'Europe', 'America'];
 const genders = ['male', 'female'];
 const browsers = ['Chrome', 'Edge'];
 
-// Generate and store user details
 const users = [];
 
 for (let i = 0; i < numUsers; i++) {
@@ -22,8 +20,7 @@ for (let i = 0; i < numUsers; i++) {
   users.push(user);
 }
 
-// Write user details to a JSON file
-fs.writeFile('person2.json', JSON.stringify(users, null, 2), (err) => {
+fs.writeFile('person4.json', JSON.stringify(users, null, 2), (err) => {
   if (err) {
     console.error('Error writing user details to file:', err);
   } else {
